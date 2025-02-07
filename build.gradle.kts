26254f88a7946868c633c1c2ab164a1beb763cd2
knight7024 <knight7024@naver.com>
2025-02-07 16:01:42 +0900

    Squashed commit of the following:
    commit 1dbb9577bf31221da19d4e9bf18f2c7951141d9e
    Author: knight7024 <knight7024@naver.com>
    Date:   Fri Feb 7 15:59:20 2025 +0900
    
        docs: PR 템플릿 설정
    
    commit 5dc3cfde544c277706a8f3e00d5cdc78c83b0cde
    Author: knight7024 <knight7024@naver.com>
    Date:   Thu Feb 6 19:10:12 2025 +0900
    
        feat: 프로젝트 시작

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+val exposed_version: String by project
+val h2_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val mongo_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.ktor.plugin") version "3.0.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-auth")
+    implementation("io.ktor:ktor-server-auth-jwt")
+    implementation("io.ktor:ktor-client-core")
+    implementation("io.ktor:ktor-client-apache")
+    implementation("io.ktor:ktor-server-cors")
+    implementation("io.ktor:ktor-server-swagger")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
+    implementation("org.mongodb:mongodb-driver-core:$mongo_version")
+    implementation("org.mongodb:mongodb-driver-sync:$mongo_version")
+    implementation("org.mongodb:bson:$mongo_version")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
