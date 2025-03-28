f1d99597c17c6c3f15f5589f5280541c71a0ed0b
knight7024 <knight7024@naver.com>
2025-03-28 18:16:34 +0900

    chore: 필요없는 라우팅 제거

--- src/main/kotlin/module/Routing.kt
+++ src/main/kotlin/module/Routing.kt
@@ -8,9 +8,6 @@ import com.example.domain.user.UserAuthorizationService
 import com.example.route.auth.authorization
 import com.example.route.extension.stashSetting
 import io.ktor.server.application.Application
-import io.ktor.server.auth.AuthenticationStrategy
-import io.ktor.server.auth.authenticate
-import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -25,13 +22,6 @@ internal fun Application.configureRouting(
     routing {
         get("/") {
             call.respondText("Hello World!")
-        }
-
-        authenticate("auth-bearer", strategy = AuthenticationStrategy.Required) {
-            get("/auth") {
-                val user = call.authenticatedUser()
-                call.respond(user.email)
-            }
         }
 
         authorization(
