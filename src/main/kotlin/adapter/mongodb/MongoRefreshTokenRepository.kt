faa712aa570eff7db99b40fc34b02651b01135d8
JongWoo Jeong <knight7024@naver.com>
2025-06-30 00:51:38 +0900

    feat: 리프레시 토큰 탈취 케이스별 대응을 개선한다. (#39)

--- src/main/kotlin/adapter/mongodb/MongoRefreshTokenRepository.kt
+++ src/main/kotlin/adapter/mongodb/MongoRefreshTokenRepository.kt
@@ -1,7 +1,10 @@
 package com.example.adapter.mongodb
 
+import arrow.core.Either
+import arrow.core.raise.either
 import com.example.domain.token.RefreshToken
 import com.example.domain.token.RefreshTokenRepository
+import com.example.domain.token.RefreshTokenRepository.InvalidateOnceError
 import com.example.domain.token.TokenId
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.model.Filters
@@ -16,25 +19,30 @@ class MongoRefreshTokenRepository(
         dao.insertOne(token.toDocument())
     }
 
-    override suspend fun invalidateOnce(token: RefreshToken): Boolean {
-        val saved =
-            dao
-                .find(
-                    Filters.eq(RefreshTokenDocument.FIELD_VALUE, token.value),
-                ).first()
-        if (saved == null || saved.status != null) {
-            return false
-        }
+    override suspend fun invalidateOnce(token: RefreshToken): Either<InvalidateOnceError, Unit> =
+        either {
+            val saved =
+                dao
+                    .find(
+                        Filters.eq(RefreshTokenDocument.FIELD_VALUE, token.value),
+                    ).first()
+            when {
+                // 탈취된 토큰으로 판단되어 이미 제거된 경우
+                saved == null -> raise(InvalidateOnceError.Purged)
 
-        return dao
-            .updateOne(
-                Filters.and(
-                    Filters.eq(RefreshTokenDocument.FIELD_VALUE, token.value),
-                    Filters.eq(RefreshTokenDocument.FIELD_STATUS, null),
-                ),
-                Updates.set(RefreshTokenDocument.FIELD_STATUS, "INVALIDATED"),
-            ).modifiedCount != 0L
-    }
+                // 재사용할 수 없는 토큰인 경우
+                saved.status != null -> raise(InvalidateOnceError.Invalidated)
+            }
+
+            dao
+                .updateOne(
+                    Filters.and(
+                        Filters.eq(RefreshTokenDocument.FIELD_VALUE, token.value),
+                        Filters.eq(RefreshTokenDocument.FIELD_STATUS, null),
+                    ),
+                    Updates.set(RefreshTokenDocument.FIELD_STATUS, "INVALIDATED"),
+                )
+        }
 
     override suspend fun invalidateAll(tokenId: TokenId) {
         dao.deleteMany(
--- src/testFixtures/kotlin/adapter/FakeRefreshTokenRepository.kt
+++ src/testFixtures/kotlin/adapter/FakeRefreshTokenRepository.kt
@@ -1,8 +1,12 @@
 package adapter
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.example.adapter.mongodb.RefreshTokenDocument
 import com.example.domain.token.RefreshToken
 import com.example.domain.token.RefreshTokenRepository
+import com.example.domain.token.RefreshTokenRepository.InvalidateOnceError
 import com.example.domain.token.TokenId
 
 class FakeRefreshTokenRepository : RefreshTokenRepository {
@@ -12,15 +16,15 @@ class FakeRefreshTokenRepository : RefreshTokenRepository {
         refreshTokens[token.value] = token.toDocument()
     }
 
-    override suspend fun invalidateOnce(token: RefreshToken): Boolean {
-        val saved = refreshTokens[token.value]
-        if (saved == null || saved.status != null) {
-            return false
+    override suspend fun invalidateOnce(token: RefreshToken): Either<InvalidateOnceError, Unit> {
+        val saved = refreshTokens[token.value] ?: return InvalidateOnceError.Purged.left()
+        if (saved.status != null) {
+            return InvalidateOnceError.Invalidated.left()
         }
 
         refreshTokens[token.value] = saved.copy(status = "INVALIDATED")
 
-        return true
+        return Unit.right()
     }
 
     override suspend fun invalidateAll(tokenId: TokenId) {
